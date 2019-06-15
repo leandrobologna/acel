6f304db620cbcb9fd84b3301ff010a16f7def8ba
Leandro Bologna <leandro.bologna@gmail.com>
2019-06-14 22:16:08 -0300

    modified:   submit.sh

--- bash/submit.sh
+++ bash/submit.sh
@@ -7,33 +7,33 @@ function pause(){
 
 clear
 
-echo "Extração dos dados de inspeção dos estabelecimentos"
+figlet "Extract :"
 echo
 pause "Pressione ENTER continuar"
 
 
 $SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster /home/labdata/acel_consulting/pyspark/extract_boston_food_establishment_inspection.py
 
-echo "Extração dos estabelecimentos ativos"
-echo
-pause "Pressione ENTER continuar"
-
 $SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster /home/labdata/acel_consulting/pyspark/extract_boston_active_food_establishment.py
 
-echo "Tratamento dos Dados Extraídos"
+hdfs dfs -ls boston_food_establishment_inspections
+
+hdfs dfs -ls boston_active_food_establishment
+
+echo
+figlet "Transform :"
 echo
 pause "Pressione ENTER continuar"
 
 $SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster /home/labdata/acel_consulting/pyspark/transformation_boston.py
 
-echo "Aplicação do Modelo nos Dados Tratados"
+beeline -u jdbc:hive2://elephant:10000 -n labdata -e "SELECT * FROM inspections_historic LIMIT 10"
+
+echo
+figlet "Predict :"
 echo
 pause "Pressione ENTER continuar"
 
 $SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster /home/labdata/acel_consulting/pyspark/predict.py
 
-echo "Exibição dos Dados Previstos Gravados no HIVE"
-echo
-pause "Pressione ENTER continuar"
-
-beeline -u jdbc:hive2://elephant:10000 -n labdata -e "SELECT * FROM predict"
+beeline -u jdbc:hive2://elephant:10000 -n labdata -e "SELECT businessname, address, descript, city, proba FROM predict LIMIT 30"
